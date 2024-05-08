1bf804fd2e33bf5644578df76632640a74afcda5
Scruffy8 <wc3zombiezach8@gmail.com>
2024-05-08 14:10:52 -0500

    Fix License and Route

--- Cryptomus/routes.php
+++ Cryptomus/routes.php
@@ -1,5 +1,4 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-//Route::post('/stripe/webhook', [App\Extensions\Gateways\Stripe\Stripe::class, 'webhook']);
 Route::post('/cryptomus/webhook', [App\Extensions\Gateways\Cryptomus\Cryptomus::class, 'webhook']);
